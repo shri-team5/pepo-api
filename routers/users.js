8dc4dec7f4bacb309229134730cc87e2f20e6f35
vorobeez <vorobeez@gmail.com>
2016-08-27 15:04:06 +0300

    реализован #74

--- routers/users.js
+++ routers/users.js
@@ -5,6 +5,8 @@ const usersController = require('../controllers/users');
 const router = Router();
 
 router.get('/:id', usersController.getUser);
+router.get('/vkontakte/:id', usersController.getUserByVkontakteId);
+router.get('/facebook/:id', usersController.getUserByFacebookId);
 router.put('/', usersController.createUser);
 
 module.exports = router;
