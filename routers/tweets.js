505c50220e2b7f28ec07c75442ad84a8ac117d07
grimfri <destiny.mail0@gmail.com>
2016-08-28 01:09:15 +0300

    Просмотр всех твитов

--- routers/tweets.js
+++ routers/tweets.js
@@ -5,6 +5,7 @@ const tweetsController = require('../controllers/tweets');
 const router = Router();
 
 router.get('/', tweetsController.getFeed);
+router.get('/world', tweetsController.getWorldFeed);
 router.post('/', tweetsController.createTweet);
 
 module.exports = router;
