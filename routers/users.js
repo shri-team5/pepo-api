fbb22d2ab93e6a8a1d573fb0e33c99bef888b87c
Andrey <andreyetumyan@gmail.com>
2016-08-27 19:17:46 +0300

    Users::getUserByUsername

--- routers/users.js
+++ routers/users.js
@@ -5,6 +5,7 @@ const usersController = require('../controllers/users');
 const router = Router();
 
 router.get('/:id', usersController.getUser);
+router.get('/username/:username', usersController.getUserByUsername);
 router.get('/vkontakte/:id', usersController.getUserByVkontakteId);
 router.get('/facebook/:id', usersController.getUserByFacebookId);
 router.post('/', usersController.createUser);
