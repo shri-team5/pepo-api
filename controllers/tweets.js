ef0df0fac6bc13816c797bdab624b1e350ac1693
Grimfri <destiny.mail0@gmail.com>
2016-09-01 21:42:55 +0300

    Replies

--- controllers/tweets.js
+++ controllers/tweets.js
@@ -2,7 +2,7 @@ const User = require('../models/User');
 const Tweet = require('../models/Tweet');
 
 function getFeed(req, res) {
-    const { userId, offset, count, ownTweetsOnly } = req.query;
+    const {userId, offset, count, ownTweetsOnly} = req.query;
 
     if (!userId) {
         return res.sendStatus(400);
@@ -11,10 +11,10 @@ function getFeed(req, res) {
     User.findById(userId).exec()
         .then(user => {
             const userIds = ownTweetsOnly === 'true' ? [userId] : [...user.subscriptions, userId];
-            return Tweet.find({ author: { $in: userIds } }, null, {
+            return Tweet.find({author: {$in: userIds}}, null, {
                 skip: +offset,
                 limit: +count,
-                sort: { createdAt: 'desc' }
+                sort: {createdAt: 'desc'}
             }).populate('author').exec();
         })
         .then(tweets => {
@@ -26,12 +26,12 @@ function getFeed(req, res) {
 }
 
 function getWorldFeed(req, res) {
-    const { offset, count } = req.query;
+    const {offset, count} = req.query;
 
     Tweet.find({}, null, {
         skip: +offset,
         limit: +count,
-        sort: { createdAt: 'desc' }
+        sort: {createdAt: 'desc'}
     }).populate('author').exec()
         .then(tweets => {
             res.send(tweets);
@@ -42,9 +42,9 @@ function getWorldFeed(req, res) {
 }
 
 function createTweet(req, res) {
-    const { userId, text, type } = req.body;
+    const {userId, text, type} = req.body;
 
-    if(!userId) {
+    if (!userId) {
         return res.sendStatus(400);
     }
 
@@ -65,8 +65,31 @@ function createTweet(req, res) {
         });
 }
 
+function getReplies(req, res) {
+
+    const {offset, count} = req.query;
+    const {tweetId} = req.params;
+
+    if (!tweetId) {
+        return res.sendStatus(400);
+    }
+
+    Tweet.find({ parentTweet: tweetId }, null, {
+        skip: +offset,
+        limit: +count,
+        sort: {createdAt: 'desc'}
+    }).populate('author').exec()
+        .then(tweets => {
+            res.send(tweets);
+        })
+        .catch(() => {
+            res.sendStatus(404);
+        });
+}
+
 module.exports = {
     getFeed,
     getWorldFeed,
-    createTweet
+    createTweet,
+    getReplies
 };
