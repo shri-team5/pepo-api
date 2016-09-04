e47368452c4897f6c3f88605efbe47d9fa1ee5f5
Artur <destiny.mail0@gmail.com>
2016-09-04 18:10:45 +0300

    Заменил s3 на cloudinary, пофиксил загрузку файлов (#28)
    * Заменил s3 на cloudinary, пофиксил загрузку файлов
    
    * Добавил в gitignore папку tmp

--- config/environments/production.js
+++ config/environments/production.js
@@ -3,10 +3,9 @@ module.exports = {
     mongodb: {
         uri: 'mongodb://root:root@ds063725.mlab.com:63725/shri-pepo'
     },
-    s3: {
-        access_key_id: 'AKIAIH6JKXTJ7QOWYGYQ',
-        secret_access_key: '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',
-        bucket_name: 'shri-pepo',
-        region: 'eu-central-1'
+    cloudinary: {
+        cloud_name: 'djwtkcbda',
+        access_key_id: '866244946745373',
+        secret_access_key: 'oCAVN0NAK4-hU76JLH0J3krpnt0'
     }
 };
